b1d7e48f3ec4ffbd2e01c1c69ab2dda6f2def752
Guillaume Coubard <guillaume.coubard@yahoo.fr>
2024-11-27 07:47:16 +0100

    Image and Tag models. Postgresql db. Models migration

--- sonor/models.py
+++ sonor/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = "Catégorie"
+
+    name = models.CharField(
+        verbose_name="nom", unique=True, null=False, blank=False, max_length=100
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    name = models.CharField(
+        verbose_name="nom", unique=True, null=False, blank=False, max_length=100
+    )
+
+    description = models.TextField(null=True, blank=True, max_length=300)
+
+    image = models.ImageField(
+        # images will be uploaded to /media/uploads/products
+        upload_to="uploads/products/"
+    )
+
+    tags = models.ManyToManyField(
+        to=Tag,
+    )
+
+    def __str__(self):
+        return self.name
